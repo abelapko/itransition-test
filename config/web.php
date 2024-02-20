41f75396ab922230f9f0067929044559675dec2f
Aleksey Belapko <alex.belapko@gmail.com>
2024-02-20 22:38:45 +0300

    install yii

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'SP8_EdagZJUPYNbMSrbYSl-wk6B-sGIy',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
