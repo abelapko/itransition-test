38fe9c4475efd234a4b2189c97f76d2c141861dd
Aleksey Belapko <alex.belapko@gmail.com>
2024-02-21 12:44:19 +0300

    load env file for web index

--- web/index-test.php
+++ web/index-test.php
@@ -11,6 +11,10 @@ defined('YII_ENV') or define('YII_ENV', 'test');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+// Looing for .env at the root directory
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 $config = require __DIR__ . '/../config/test.php';
 
 (new yii\web\Application($config))->run();
