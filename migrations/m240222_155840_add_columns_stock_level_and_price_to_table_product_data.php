8561fcdacce71d5422481b3b09a2fd276f75fc52
Aleksey Belapko <alex.belapko@gmail.com>
2024-02-22 19:36:35 +0300

    add migration with new columns: stock level and price

---
+++ migrations/m240222_155840_add_columns_stock_level_and_price_to_table_product_data.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Migration;
+
+class m240222_155840_add_columns_stock_level_and_price_to_table_product_data extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('tblProductData', 'intStockLevel', $this->integer()->unsigned()->notNull());
+        $this->addColumn('tblProductData', 'decPrice', $this->decimal(10, 2)->unsigned()->notNull());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('tblProductData', 'intStockLevel');
+        $this->dropColumn('tblProductData', 'decPrice');
+    }
+}
